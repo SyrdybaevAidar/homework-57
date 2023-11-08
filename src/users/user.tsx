86d08091292cf7d17857e808b69633e23525dbe5
aidar_syrdybaev <ajdarsyrdybaev@gmail.com>
2023-11-08 20:40:46 +0600

    add user component

---
+++ src/users/user.tsx
@@ -0,0 +1,17 @@
+import React  from "react";
+
+interface userProps extends React.PropsWithChildren {
+    Name: string;
+    Email: string;
+    Role: string;
+    IsActive: boolean;
+}
+
+export const user = (props: userProps) => {
+    return (<div>
+        <p>{props.Name}</p>
+        <p>{props.Email}</p>
+        <p>{props.Role}</p>
+        <p>{props.IsActive}</p>
+    </div>);
+};
