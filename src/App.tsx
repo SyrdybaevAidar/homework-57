74b27b53b46163bcb52b29941e89d9d71202571a
aidar_syrdybaev <ajdarsyrdybaev@gmail.com>
2023-11-09 18:33:46 +0600

    refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
-import { UserModel, Create } from './users/create';
+import Create from './users/create';
+import {UserModel} from './users/create';
 import { Users } from './users/user';
 import './App.css'
 
 const App = () => {
   const [users, setUsers] = useState(new Array<UserModel>());
-  const addUser = (user: UserModel) => {
+  const [createdUser, setUser] = useState(new UserModel("","", 0, false));
+  const addUser = () => {
     const usersCopy = [...users];
-    usersCopy.push(user);
+    usersCopy.push(createdUser);
+    setUser(new UserModel("","", 0, false));
     setUsers(usersCopy);
   };
 
   return (
     <div>
-      <Create CreateHandler={addUser}/>
+      <Create CreatedUser={createdUser} CreateHandler={addUser} SetHandler={setUser}/>
       <Users users={users}/>
     </div>
   )
--- src/users/Create.tsx
+++ src/users/Create.tsx
@@ -1,66 +1,91 @@
-import React, { FormEvent }  from "react";
-import { useState } from 'react';
-import {Role} from "./Role";
+import React, { useState } from 'react';
+import { Role } from './Role';
 
-export class UserModel{
+export class UserModel {
     Name: string;
     Email: string;
     RoleId: number;
     IsActive: boolean;
-    constructor(name: string, email:string, roleId: number, isActive: boolean){
+
+    constructor(name: string, email: string, roleId: number, isActive: boolean) {
         this.Name = name;
         this.Email = email;
         this.RoleId = roleId;
         this.IsActive = isActive;
-    };
+    }
 
-    setName(name: string): UserModel{
+    setName(name: string): UserModel {
         this.Name = name;
         return this;
     }
 
-    setEmail(email: string): UserModel{
+    setEmail(email: string): UserModel {
         this.Email = email;
         return this;
     }
 
-    setRoleId(roleId: number): UserModel{
+    setRoleId(roleId: number): UserModel {
         this.RoleId = roleId;
         return this;
     }
 
-    setIsActive(isActive: boolean): UserModel{
+    setIsActive(isActive: boolean): UserModel {
         this.IsActive = isActive;
         return this;
     }
-};
-
-interface CreateProps extends React.PropsWithChildren{
-    CreateHandler: (user: UserModel) => void;
 }
 
-export const Create = (props: CreateProps) => {
-    const [createdUser, setCreatedUser] = useState(new UserModel("", "", 0, false));
-    const roleComponents = Role.Roles.map(it => 
-        (
-            <option value={it.Id}>{it.Name}</option>
-        )
-    );
+interface CreateProps {
+    CreatedUser: UserModel;
+    CreateHandler: () => void;
+    SetHandler: (user: UserModel) => void;
+}
+
+const Create: React.FC<CreateProps> = (props) => {
+    const [localUser, setLocalUser] = useState<UserModel>(props.CreatedUser);
+
+    const roleComponents = Role.Roles.map((role) => (
+        <option key={role.Id} value={role.Id}>
+            {role.Name}
+        </option>
+    ));
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value, type } = event.target;
+        const updatedUser = new UserModel(
+            localUser.Name,
+            localUser.Email,
+            localUser.RoleId,
+            localUser.IsActive
+        );
+
+        if (type === 'checkbox') {
+            updatedUser.setIsActive(!localUser.IsActive);
+        } else {
+            if (name === 'Name') updatedUser.setName(value);
+            else if (name === 'Email') updatedUser.setEmail(value);
+            else if (name === 'RoleId') updatedUser.setRoleId(parseInt(value));
+        }
+
+        setLocalUser(updatedUser);
+    };
 
     const addUser = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        props.CreateHandler(createdUser);
-        setCreatedUser(new UserModel("", "", 0, false));
+        props.CreateHandler();
     };
 
     return (
-        <form method="post" onSubmit={e => addUser(e)}>
-            <input type="text" onChange={e => setCreatedUser(createdUser.setName(e.currentTarget.value))}/>
-            <input type="text" onChange={e => setCreatedUser(createdUser.setEmail(e.currentTarget.value))}/>
-            <input type="checkbox" onChange={e => setCreatedUser(createdUser.setIsActive(e.currentTarget.checked))}/>
-            <select onChange={e => setCreatedUser(createdUser.setRoleId(parseInt(e.currentTarget.value)))}>
+        <form method="post" onSubmit={addUser} onInput={() => props.SetHandler(localUser)}>
+            <input type="text" name="Name" value={localUser.Name} onChange={handleInputChange} />
+            <input type="text" name="Email" value={localUser.Email} onChange={handleInputChange} />
+            <input type="checkbox" name="IsActive" checked={localUser.IsActive} onChange={handleInputChange} />
+            <select name="RoleId" onChange={handleInputChange}>
                 {roleComponents}
             </select>
-            <button type="submit"></button>
-        </form>);
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
+
+export default Create;
